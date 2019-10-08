03b049c6bcf24c33f02e987a80ee4397cdf55866
Roberto Nobrega <rwnobrega@gmail.com>
2019-10-08 17:25:47 -0300

    Arrumando bugs

--- Trabalho#1/MatPRE/Questao01.m
+++ Trabalho#1/MatPRE/Questao01.m
@@ -2,7 +2,6 @@
 clear all;
 close all;
 clc;
-rand_binom(510,0.5);                 
 
 
 %% Questão 1.b
@@ -10,7 +9,7 @@ rand_binom(510,0.5);
 clear all;
 close all;
 clc;
-NExp = 1000000;
+NExp = 100000;
 n_01 = 10;
 n_02 = 50;
 p_01 = 0.1;
@@ -25,35 +24,37 @@ for i = 1:NExp
 end
 % -------Teórico01-------------------
 vetTeorico01 = 0:n_01;
-for k = 1: n_01+1
-    vetTeorico01(k) =  pmf_binom(n_01,k,p_01);  
+for k = vetTeorico01
+    vetTeorico01(k+1) =  pmf_binom(n_01,k,p_01);  
 end
 % -------Teórico02-------------------------
 vetTeorico02 = 0:n_02;
 
-for k = 1 : n_02+1
-    vetTeorico02(k) =  pmf_binom(n_02,k,p_02);  
+for k = vetTeorico02
+    vetTeorico02(k+1) =  pmf_binom(n_02,k,p_02);  
 end
 %---------Plot experimento 01---------
 histograma_01 = hist(vet01,0:n_01);
 pmf_01_simul = histograma_01 / NExp;
 
-figure; 
+figure 1; 
 bar(0:n_01, pmf_01_simul,'b');
+hold on;
 xlim([-2 max(vet01)+2]);
 %--------------Plot experimento 02----
 histograma_02 = hist(vet02,0:n_02);
 pmf_02_simul = histograma_02 / NExp;
 
-figure;
+figure 2;
 bar(0:n_02, pmf_02_simul,'r');
+hold on;
 xlim([-2 max(vet02)+2]);
 %----------Plot teorico 01---------------
-figure
+figure 1
 stem(0:n_01,vetTeorico01);
 xlim([-2 max(n_01)+2]);
 %----------Plot teorico 02---------------
-figure
+figure 2
 stem(0:n_02,vetTeorico02);
 xlim([-2 max(n_02)+2]);
 %  figure(2);
--- Trabalho#1/MatPRE/pmf_binom.m
+++ Trabalho#1/MatPRE/pmf_binom.m
@@ -1,5 +1,9 @@
 function PK =  pmf_binom (n,k,p)
+    
+    n
+    k
+    p
 
-    PK = ( factorial(abs(n)) / (factorial(abs(k)) .* factorial(abs(n-k)))) .* ( (p .^ k).* ((1-p).^ (n-k)) );
+    PK = ( factorial(n) / (factorial(k) .* factorial(n-k))) .* ( (p .^ k).* ((1-p).^ (n-k)) );
     
 return  
